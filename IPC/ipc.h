0a42f0e9eaf0cd8122e0914c5691750ff6244c95
studentTxl <studentTxl@163.com>
2017-10-29 22:09:53 +0800

    ADD IPC

---
+++ IPC/ipc.h
@@ -0,0 +1,77 @@
+/*************************************************************************
+    > File Name: ipc.h
+    > Author: tongxianglong
+    > Mail: studentTxl@163.com 
+    > Created Time: Sun 29 Oct 2017 12:07:40 PM CST
+ *************************************************************************
+
+
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/ipc.h>
+#include<stdlib.h>
+#include<sys/sem.h>
+#include<sys/shm.h>
+
+union semun
+{
+	int val;
+	struct semid_ds *buf;
+	unsigned short *array;
+	struct seminfo *_buf;
+};
+
+//////////////////////////////////////////////////
+//1  ftok
+
+key_t Ftok(const char *pathname, int pro_id)
+{
+	key_t key = ftok(pathname, proj_id);
+	if(key == -1)
+	{
+		perror("ftok.");
+		exit(1);
+	}
+	return key;
+}
+
+///////////////////////////////////////////////////
+//2   semget
+
+int Semget(key_t key, int nsems, int semflg)
+{
+	int id = semget(key, nsems, semflg);
+	if(id == -1)
+	{
+		perror("semget.");
+		exit(1);
+	}
+	return id;
+}
+
+///////////////////////////////////////////////////
+//3 Shmget
+
+int Shmget(key_t key, size_t size, int shmflg)
+{
+	int id = shmget(key, size, shmflg);
+	if(id == -1)
+	{
+		perror("shmget.");
+		exit(1);
+	}
+	return id;
+}
+
+void *Shmat(int shmid, const void *shmaddr, int shmflg)
+{
+	void *addr = shmat(shmid, shmaddr, shmflg);
+	if(addr == (void*)-1)
+	{
+		perror("shmat.");
+		exit(1);
+	}
+	return addr;
+}
+
+
---
+++ IPC/sem/create_sem.c
@@ -0,0 +1,24 @@
+#include"../ipc.h"
+#include<sys/sem.h>
+
+// ./create_sem mysem 0xff n
+
+int main(int argc, char *argv[])
+{
+	if(argc != 4)
+	{
+		printf("<Waring:Using> %s pathname proj_id init_val.\n>",argv[0]);
+		return -1;
+	}
+	key_t sem_key = Ftok(argv[1], atoi(argv[2]));
+	printf("sem key = %x\n", sem_key);
+
+	ini sem_id = Semget(sem_key, 1, IPC_CREAT|IPC_EXCL|0755);
+	printf("sem id = %d\n", sem_id);
+
+	union semun init;
+	init.val = atoi(argv[3]);
+	semctl(sem_id, 0, SETVAL, init);
+
+	return 0;
+}
