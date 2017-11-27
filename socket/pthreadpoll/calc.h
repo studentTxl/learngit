a12f290dfcc1ab5318aac63fbb7e1fb893506284
studentTxl <studentTxl@163.com>
2017-11-27 20:49:50 +0800

    add threadpool

--- socket/pthreadpoll/calc.h
+++ socket/pthreadpoll/calc.h
@@ -2,29 +2,21 @@
     > File Name: calc.h
     > Author: tongxianglong
     > Mail: studentTxl@163.com 
-    > Created Time: Sat 18 Nov 2017 03:30:09 PM CST
- *************************************************************************/
+    > Created Time: Sun 26 Nov 2017 05:39:38 PM CST
+ ************************************************************************/
 
 #ifndef _CALC_H
 #define _CALC_H
 
 #include<stdio.h>
 #include<unistd.h>
-#include<sys/socket.h>
 #include<netinet/in.h>
 #include<arpa/inet.h>
+#include<sys/socket.h>
 
+typedef enum {QUIT, ADD, SUB, MUL, DIV, MOD}ENUM_CALC_TYPE;
 
 #define CMD_SIZE 5
-typedef enum
-{
-	QUIT,
-	ADD,
-	SUB,
-	MUL,
-	DIV,
-	MOD
-}ENUM_CALC_TYPE;
 
 typedef struct calcst
 {
---
+++ socket/pthreadpoll/socket.h
@@ -0,0 +1,60 @@
+/*************************************************************************
+    > File Name: socket.h
+    > Author: tongxianglong
+    > Mail: studentTxl@163.com 
+    > Created Time: Sun 26 Nov 2017 05:49:25 PM CST
+ ************************************************************************/
+
+#ifndef _SOCKET_H
+#define _SOCKET_H
+
+#define TCP 0
+#define UDP 1
+#define LISTEN_QUEUE_SIZE 5
+
+int start_up(char *ip, short port, int mode)
+{
+	int sockfd;
+	if(mode == TCP)
+	{
+		sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	}
+	else if(mode == UDP)
+	{
+		sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	}
+	else
+	{
+		printf("mode error, mode is must tcp or udp!\n");
+		return -1;
+	}
+
+	struct sockaddr_in address;
+	address.sin_family = AF_INET;
+	address.sin_port = htons(port);
+	address.sin_addr.s_addr = inet_addr(ip);
+
+	socklen_t len = sizeof(struct sockaddr);
+
+	int yes = 1;
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+
+	int ret = bind(sockfd, (struct sockaddr *)&address, len);
+	if(ret == -1)
+	{
+		perror("bind.");
+		return -1;
+	}
+	
+	if(mode == TCP)
+	{
+		ret = listen(sockfd, LISTEN_QUEUE_SIZE);
+		if(ret == -1)
+		{
+			perror("listen.");
+			return -1;
+		}
+	}
+	return sockfd;
+}
+#endif
